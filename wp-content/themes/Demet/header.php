437bdae9891671e0252b49be5fc13a7f35180f05
Andrew Cobb <cobb.andrew@gmail.com>
2012-08-14 16:26:18 -0500

    Styling header.

--- wp-content/themes/Demet/header.php
+++ wp-content/themes/Demet/header.php
@@ -42,28 +42,28 @@
 	<div id="wrapper">
 		<div id="container" class="container">  
 			<div class="span-24">
-                <div class="span-20">
-    				<div id="pagemenucontainer">
-    					<?php
-                    if(function_exists('wp_nav_menu')) {
-                        wp_nav_menu( 'depth=1&theme_location=menu_1&menu_id=pagemenu&container=&fallback_cb=menu_1_default');
-                    } else {
-                        menu_1_default();
-                    }
-                    
-                    function menu_1_default()
-                    {
-                        ?>
-                        <ul id="pagemenu">
-    						<li <?php if(is_home()) { ?> class="current_page_item" <?php } ?>><a href="<?php echo get_option('home'); ?>/">Home</a></li>
-    						<?php wp_list_pages('depth=1&sort_column=menu_order&title_li=' ); ?>
-    					</ul>
-                        <?php
-                    }
-                    
-                ?>
-    				</div>
-				</div>
+                <!-- <div class="span-20">
+                            <div id="pagemenucontainer">
+                              <?php
+                                    if(function_exists('wp_nav_menu')) {
+                                        wp_nav_menu( 'depth=1&theme_location=menu_1&menu_id=pagemenu&container=&fallback_cb=menu_1_default');
+                                    } else {
+                                        menu_1_default();
+                                    }
+                                    
+                                    function menu_1_default()
+                                    {
+                                        ?>
+                                        <ul id="pagemenu">
+                                <li <?php if(is_home()) { ?> class="current_page_item" <?php } ?>><a href="<?php echo get_option('home'); ?>/">Home</a></li>
+                                <?php wp_list_pages('depth=1&sort_column=menu_order&title_li=' ); ?>
+                              </ul>
+                                        <?php
+                                    }
+                                    
+                                ?>
+                            </div>
+                        </div> -->
 			
                 <div class="span-4 last">
                     <div class="topright">
