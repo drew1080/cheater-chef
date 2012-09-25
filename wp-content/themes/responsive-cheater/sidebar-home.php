972bd81138e98eafd6f9bb31f8b7002bd3cff6ec
Andrew Cobb <cobb.andrew@gmail.com>
2012-09-25 18:39:03 -0500

    Adding new plugins. Styling homepage.

--- wp-content/themes/responsive-cheater/sidebar-home.php
+++ wp-content/themes/responsive-cheater/sidebar-home.php
@@ -37,14 +37,14 @@ if ( !defined('ABSPATH')) exit;
         <div class="grid col-300">
         <?php responsive_widgets(); // responsive above widgets hook ?>
             
-			<?php if (!dynamic_sidebar('home-widget-2')) : ?>
-            <div class="widget-wrapper">
-            
-                <div class="widget-title-home"><h3><?php _e('Home Widget 2', 'responsive'); ?></h3></div>
-                <div class="textwidget"><?php _e('This is your second home widget box. To edit please go to Appearance > Widgets and choose 7th widget from the top in area seven called Home Widget 2. Title is also manageable from widgets as well.','responsive'); ?></div>
-            
-			</div><!-- end of .widget-wrapper -->
-			<?php endif; //end of home-widget-2 ?>
+        <?php if (!dynamic_sidebar('home-widget-2')) : ?>
+              <div class="widget-wrapper">
+
+                  <div class="widget-title-home"><h3><?php _e('Home Widget 2', 'responsive'); ?></h3></div>
+                  <div class="textwidget"><?php _e('This is your third home widget box. To edit please go to Appearance > Widgets and choose 8th widget from the top in area eight called Home Widget 3. Title is also manageable from widgets as well.','responsive'); ?></div>
+
+  			</div><!-- end of .widget-wrapper -->
+  			<?php endif; //end of home-widget-2 ?>
             
         <?php responsive_widgets_end(); // after widgets hook ?>
         </div><!-- end of .col-300 -->
