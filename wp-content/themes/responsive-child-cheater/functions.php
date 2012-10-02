1cef1ce3cb6a2112bd6eaec07c3b5a5c83168200
Andrew Cobb <cobb.andrew@gmail.com>
2012-10-02 17:15:54 -0500

    Adding styling changes and functions from root theme modifications.

--- wp-content/themes/responsive-child-cheater/functions.php
+++ wp-content/themes/responsive-child-cheater/functions.php
@@ -1,35 +1,26 @@
 <?php
 
-/**
- * WordPress Widgets start right here.
- */
-function responsive_widgets_init() {
+  register_sidebar(array(
+      'name' => __('Home Feature Post', 'responsive'),
+      'description' => __('Home Feature - home.php', 'responsive'),
+      'id' => 'home-feature-post',
+      'before_title' => '<div class="widget-title-home"><h3>',
+      'after_title' => '</h3></div>',
+      'before_widget' => '<div id="cheater-featured-post" class="widget-wrapper %2$s">',
+      'after_widget' => '</div>'
+  ));
   
-    register_sidebar(array(
-        'name' => __('Home Feature Post', 'responsive'),
-        'description' => __('Home Feature - home.php', 'responsive'),
-        'id' => 'home-feature-post',
-        'before_title' => '<div class="widget-title-home"><h3>',
-        'after_title' => '</h3></div>',
-        'before_widget' => '<div id="cheater-featured-post" class="widget-wrapper %2$s">',
-        'after_widget' => '</div>'
-    ));
-    
-}
+  function add_search_box($items, $args) {
 
-add_action('widgets_init', 'responsive_widgets_init');
+         ob_start();
+         get_search_form();
+         $searchform = ob_get_contents();
+         ob_end_clean();
 
-function add_search_box($items, $args) {
+         $items .= '<li class="search-nav">' . $searchform . '</li>';
 
-        ob_start();
-        get_search_form();
-        $searchform = ob_get_contents();
-        ob_end_clean();
+     return $items;
+   }
 
-        $items .= '<li class="search-nav">' . $searchform . '</li>';
-
-    return $items;
-}
-
-add_filter('wp_nav_menu_items','add_search_box', 10, 2);
+   add_filter('wp_nav_menu_items','add_search_box', 10, 2);
 ?>
